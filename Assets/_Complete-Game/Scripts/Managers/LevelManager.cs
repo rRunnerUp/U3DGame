63ae08a56d43d6bcfb7f31e1a709a6d99d83f431
RJ <rafal.juchnicki@gmail.com>
2018-01-23 22:52:05 +0100

    Dodane wyswietlenie levelu + uzupelnilem gitignore zeby sie pliki nie pojebaly

---
+++ Assets/_Complete-Game/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CompleteProject
+{
+    public class LevelManager : MonoBehaviour
+    {
+
+        public static int level;        // The player's score.
+
+
+        Text text;                      // Reference to the Text component.
+        CanvasGroup cg;
+        public float smoothTime = 0.3F;
+
+        public static bool showLevelText;
+
+        // Use this for initialization
+        void Start()
+        {
+            showLevelText = true;
+            text = GetComponent<Text>();
+            cg = GetComponent<CanvasGroup>();
+
+            StartCoroutine(Example());
+
+            level = 1;
+           
+        }
+
+
+        IEnumerator Example()
+        {
+            if(showLevelText)
+            {
+                cg.alpha = 1;
+                yield return new WaitForSeconds(5);
+                cg.alpha = 0;
+                showLevelText = false;
+            }
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            StartCoroutine(Example());
+            text.text = "Level: " + level;
+        }
+    }
+}
